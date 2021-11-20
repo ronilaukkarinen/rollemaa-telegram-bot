eff0547acfcec98ecd2bba10aa97cc4ba40bb9c9
Roni Laukkarinen <roni@dude.fi>
2021-11-20 22:04:19 +0200

    Remove cpt specific stuff, change post id var

--- rollemaa-telegram-bot.php
+++ rollemaa-telegram-bot.php
@@ -17,15 +17,15 @@
 
 /**
  * Notify when post gets published.
- * @param string $postid Post ID
+ * @param string $post_id Post ID
  */
-function rollemaa_telegram_send_notification( $postid ) {
+function rollemaa_telegram_send_notification( $post_id ) {
 
   // Settings
   $telegram_bot_api_key = getenv( 'TELEGRAM_BOT_API_KEY' );
   $method = 'sendMessage';
   $chat_id = getenv( 'TELEGRAM_CHAT_ID' );
-  $text = 'Uusi kirjoitus julkaistu: ' . get_the_title( $postid ) . '. Linkki: ' . get_the_permalink( $postid ) . '';
+  $text = 'Uusi kirjoitus julkaistu: ' . get_the_title( $post_id ) . '. Linkki: ' . get_the_permalink( $post_id ) . '';
 
   // API call
   $url = 'https://api.telegram.org/bot' . $telegram_bot_api_key . '/' . $method . '?chat_id=' . $chat_id . '&text=' . $text;
@@ -37,45 +37,25 @@ function rollemaa_telegram_send_notification( $postid ) {
   remove_action( 'pre_post_update', 'wp_save_post_revision' );
 
   // Get postdata
-  $postdata = get_post( $postid );
+  $postdata = get_post( $post_id );
 
   // Get time difference
   $time_differ = round( abs( strtotime( $postdata->post_modified ) - strtotime( $postdata->post_date ) ) / 60, 2 );
 
-  if ( 'movie' === get_post_type( $postid ) ) {
-
-    // Bail if conditions not met
-    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
-    if ( wp_is_post_revision( $post_id ) ) return;
-    if ( wp_is_post_autosave( $post_id ) ) return;
-    if ( did_action( 'save_post' ) > 1 ) return;
-    if ( get_post_status( $postid ) === 'draft' ) return;
-    if ( get_post_status( $postid ) === 'auto-draft' ) return;
-    if ( get_post_status( $postid ) === 'private' ) return;
-    if ( get_post_status( $postid ) === 'future'  ) return;
-    if ( get_post_status( $postid ) === 'pending' ) return;
-    if ( get_post_status( $postid ) === 'trash' ) return;
-
-    // Send a message
-    if ( $time_differ < 0.10 ) {
-      wp_remote_get( $url );
-    }
-  }
-
   // Bail if other conditions not met
   if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
-  if ( empty( get_the_title( $postid ) ) ) return;
-  if ( empty( get_the_content( $postid ) ) ) return;
-  if ( empty( get_the_permalink( $postid ) ) ) return;
+  if ( empty( get_the_title( $post_id ) ) ) return;
+  if ( empty( get_the_content( $post_id ) ) ) return;
+  if ( empty( get_the_permalink( $post_id ) ) ) return;
   if ( wp_is_post_revision( $post_id ) ) return;
   if ( wp_is_post_autosave( $post_id ) ) return;
   if ( did_action( 'save_post' ) > 1 ) return;
-  if ( get_post_status( $postid ) === 'draft' ) return;
-  if ( get_post_status( $postid ) === 'auto-draft' ) return;
-  if ( get_post_status( $postid ) === 'private' ) return;
-  if ( get_post_status( $postid ) === 'future'  ) return;
-  if ( get_post_status( $postid ) === 'pending' ) return;
-  if ( get_post_status( $postid ) === 'trash' ) return;
+  if ( get_post_status( $post_id ) === 'draft' ) return;
+  if ( get_post_status( $post_id ) === 'auto-draft' ) return;
+  if ( get_post_status( $post_id ) === 'private' ) return;
+  if ( get_post_status( $post_id ) === 'future'  ) return;
+  if ( get_post_status( $post_id ) === 'pending' ) return;
+  if ( get_post_status( $post_id ) === 'trash' ) return;
 
   // Send a message
   if ( $time_differ < 0.10 ) {
